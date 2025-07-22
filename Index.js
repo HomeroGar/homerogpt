8821e93e928d9e243d360556d6517aa7d5d0cdfe
HomeroGar <homerogar11@gmail.com>
2025-07-21 22:00:56 -0600

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/webhook', (req, res) => {
+  const VERIFY_TOKEN = "MI_TOKEN_SC_TEST";
+  const mode = req.query['hub.mode'];
+  const token = req.query['hub.verify_token'];
+  const challenge = req.query['hub.challenge'];
+
+  if (mode && token === VERIFY_TOKEN) {
+    console.log("WEBHOOK_VERIFIED");
+    res.status(200).send(challenge);
+  } else {
+    res.sendStatus(403);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
