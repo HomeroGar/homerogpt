be6026a0f3da3c4dc798794c902e44fa81c7d51c
HomeroGar <homerogar11@gmail.com>
2025-07-21 22:32:48 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
+// VALIDACIÓN DE TOKEN (GET)
 app.get('/webhook', (req, res) => {
   const VERIFY_TOKEN = "MI_TOKEN_SC_TEST";
   const mode = req.query['hub.mode'];
@@ -15,6 +16,19 @@ app.get('/webhook', (req, res) => {
     res.status(200).send(challenge);
   } else {
     res.sendStatus(403);
+  }
+});
+
+// RECEPCIÓN DE EVENTOS (POST)
+app.post('/webhook', (req, res) => {
+  const body = req.body;
+
+  console.log('Webhook POST recibido:', JSON.stringify(body, null, 2));
+
+  if (body.object) {
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    res.sendStatus(404);
   }
 });
 
